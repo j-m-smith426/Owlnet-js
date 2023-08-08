d85dc1aa3c83862ac16a5b960133153436749f93
j-m-smith426 <joabsmith4226@gmail.com>
2023-08-08 15:04:11 -0400

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Col, Container, Row } from 'react-bootstrap'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container fluid className={"full"}>
+        <Row className={"nav_container"}>
+          <Col>NavBar</Col>
+        </Row>
+        <Row>
+          <Col xs={3}>Menu</Col>
+          <Col xs={6}>Content</Col>
+          <Col xs={3}>Feed</Col>
+        </Row>
+      </Container>
+
+      
     </>
   )
 }

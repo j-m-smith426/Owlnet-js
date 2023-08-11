dc7a2bc0d34624101298b6f482e7275a5c993409
j-m-smith426 <joabsmith4226@gmail.com>
2023-08-11 11:45:40 -0400

    Basic Routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 
 import { Col, Container, Row } from 'react-bootstrap'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home/Home'
 
 function App() {
 
@@ -10,13 +12,46 @@ function App() {
     <>
       <Container fluid className={"full"}>
         <Row className={"nav_container_row"}>
-          <Col className='nav_container_col'>
+          <nav className='nav_container_col'>
             <NavBar />
-          </Col>
+          </nav>
         </Row>
         <Row>
-          <Col xs={3}>Menu</Col>
-          <Col xs={6}>Content</Col>
+          <Col xs={3}>
+            <nav className="sidebar">
+              <ul>
+                <li>
+                  <Link to={"/"}>
+                      Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/follow"}>
+                    Following
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/setting"}>
+                    Settings
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/discover"}>
+                    Discover
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </Col>
+          <Col xs={6}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/follow' element={<Home />} />
+              <Route path='/setting' element={<Home />} />
+              <Route path='/discover' element={<Home />} />
+              <Route path='/messages' element={<Home />} />
+            </Routes>
+          </Col>
           <Col xs={3}>Feed</Col>
         </Row>
       </Container>
